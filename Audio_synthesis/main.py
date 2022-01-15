4335d1e0682c21c715853b8653a0699c5e5b2ee8
Interweave <interweave@qq.com>
2022-01-15 13:19:50 +0800

    Initial commit

---
+++ Audio_synthesis/main.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+from aip import AipSpeech
+
+APP_ID = '25517271'
+API_KEY = 'DvbjZrHv7eKDYiRnvjYUZajX'
+SECRET_KEY = 'eZWXWqDdMBwWIPxBU7g5mvi2sis2isBo'
+client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
+for i in range(1):
+    if i == 0:
+        content = '模型加载成功'
+        result = client.synthesis(content, 'zh', 1, {'spd': 6, 'vol': 5, 'per': 3})
+    # 识别正确返回语音二进制 错误则返回dict 参照下面错误码
+    filename = str(i)
+    if not isinstance(result, dict):
+        with open(filename + '.mp3', 'wb') as f:
+            f.write(result)
